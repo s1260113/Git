5018c5e9bf550d8b8de0b4d880c685b6935f227a
国分俊也 <kokubunshunya@shunya-pro.local>
2019-07-09 03:24:44 +0900

    first revision

---
+++ dicegame.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+int main () {
+int a,b,c;
+srand(time(NULL));
+a=rand()%6+1;
+b=rand()%6+1;
+c=a+b;
+printf("Rolling the dice...\nDie 1: %d\nDie 2: %d\nTotal value: %d\n",a,b,c);
+return 0;
+}
